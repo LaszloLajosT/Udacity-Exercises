82ba521c3e1f93fa31e7d96d51db757629510fad
LaszloLajosT <track.runner800@outlook.com>
2020-06-27 00:03:33 +0100

    Fix SettingsFragment

--- Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -14,9 +14,5 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
         addPreferencesFromResource(R.xml.pref_visualizer);
     }
-
-
-    // TODO (7) Set the root layout of activity_settings to our newly created SettingsFragment
-    // and remove the padding.
-    // [HINT] Use a <fragment> element in xml
+    
 }
