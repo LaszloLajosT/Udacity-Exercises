48fbeea2c2229237bba06e2a0d6f177a7a3a4e20
LaszloLajosT <track.runner800@outlook.com>
2020-07-29 20:19:17 +0100

    Add Executors

--- Lesson09b-ToDo-List-AAC/T09b.04-Exercise-Executors/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.04-Exercise-Executors/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -18,6 +18,8 @@ package com.example.android.todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.android.todolist.database.TaskEntry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -27,6 +29,8 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import android.view.View;
 
 import com.example.android.todolist.database.AppDatabase;
+
+import java.util.List;
 
 import static androidx.recyclerview.widget.DividerItemDecoration.VERTICAL;
 
@@ -105,13 +109,26 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     @Override
     protected void onResume() {
         super.onResume();
-        // TODO (5) Get the diskIO Executor from the instance of AppExecutors and
+        //  COMPLETED (5) Get the diskIO Executor from the ins tance of AppExecutors and
         // call the diskIO execute method with a new Runnable and implement its run method
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                final List<TaskEntry> tasks = mDb.taskDao().loadAllTasks();
+                // We will be able to simplify this once we learn more
+                // about Android Architecture Components
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.setTasks(tasks);
+                    }
+                });
+            }
+        });
 
-        // TODO (6) Move the logic into the run method and
+        // COMPLETED (6) Move the logic into the run method and
         // extract the list of tasks to a final variable
-        // TODO (7) Wrap the setTask call in a call to runOnUiThread
-        mAdapter.setTasks(mDb.taskDao().loadAllTasks());
+        // COMPLETED (7) Wrap the setTask call in a call to runOnUiThread
     }
 
     @Override
