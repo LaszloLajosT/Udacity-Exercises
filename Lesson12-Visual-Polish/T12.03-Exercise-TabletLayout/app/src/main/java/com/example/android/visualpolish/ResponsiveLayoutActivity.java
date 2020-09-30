02a315dd67cce003163b1c029e3e607b4dece76c
LaszloLajosT <track.runner800@outlook.com>
2020-09-30 22:04:22 +0100

    Migrate AndroidX

--- Lesson12-Visual-Polish/T12.03-Exercise-TabletLayout/app/src/main/java/com/example/android/visualpolish/ResponsiveLayoutActivity.java
+++ Lesson12-Visual-Polish/T12.03-Exercise-TabletLayout/app/src/main/java/com/example/android/visualpolish/ResponsiveLayoutActivity.java
@@ -1,8 +1,7 @@
 package com.example.android.visualpolish;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ResponsiveLayoutActivity extends AppCompatActivity {
 
